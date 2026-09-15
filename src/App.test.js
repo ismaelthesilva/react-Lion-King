4ef3a5144b3d2aa19027b511c5da62d8e486cf7e
Ismael Silva <ismaelsilva@icloud.com>
2026-09-16 10:18:15 +1200

    feat: build and update project ofr tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders newsletter subscribe form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/subscribe to our newsletter/i);
+  expect(heading).toBeInTheDocument();
 });
